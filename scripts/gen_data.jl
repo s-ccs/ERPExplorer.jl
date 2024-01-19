0ddbfe92e2c303f41e0e47aa3b37cffbaa0bdb8a
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-01-19 15:02:53 +0000

    better names for simulated data

--- scripts/gen_data.jl
+++ scripts/gen_data.jl
@@ -1,16 +1,16 @@
 function gen_data()
-    d1, evts = UnfoldSim.predef_eeg(noiselevel=25; return_epoched=true)
+    d1, evts = UnfoldSim.predef_eeg(n_repeats=120, noiselevel=25; return_epoched=true)
     dataS = permutedims(repeat(d1, 1, 1, 64), (3, 1, 2))
     dataS = dataS .+ rand(dataS)
 
     evts = insertcols(evts,
-        :continuous2 => rand(nrow(evts)),
-        :continuous3 => rand(nrow(evts)),
-        :continuous4 => rand(nrow(evts)),
-        :continuous5 => rand(nrow(evts)), :condition2 => shuffle(repeat(["string1", "string2"], outer=div(nrow(evts), 2))),
-        :condition3 => shuffle(repeat(["cat", "dog"], outer=div(nrow(evts), 2))),
-        :condition4 => shuffle(repeat(["orange", "banana"], outer=div(nrow(evts), 2))),
-        :condition5 => shuffle(repeat(["black", "white"], outer=div(nrow(evts), 2))))
+        :saccade_amplitude => rand(nrow(evts)) .* 15,
+        :luminance => rand(nrow(evts)) .* 100,
+        :contrast => rand(nrow(evts)),
+        :string => shuffle(repeat(["stringsuperlong", "stringshort", "stringUPPERCASE", "stringEXCITED!!!!"], outer=div(nrow(evts), 4))),
+        :animal => shuffle(repeat(["cat", "dog", "monkey"], outer=div(nrow(evts), 3))),
+        :fruit => shuffle(repeat(["orange", "banana"], outer=div(nrow(evts), 2))),
+        :color => shuffle(repeat(["black", "white"], outer=div(nrow(evts), 2))))
 
     return dataS, evts
 end
