2f3f677ce6ac5286d8629cd853cfbfc26668a458
Vladimir Mikheev <dezem@bk.ru>
2024-01-22 19:39:49 +0000

    libraries

--- scripts/example.jl
+++ scripts/example.jl
@@ -1,7 +1,13 @@
 using Revise
 using ERPExplorer
+using UnfoldSim
+using DataFrames
+using Random
+using Unfold
+using Bonito
 
-includet("gen_data.jl")
+
+include("gen_data.jl")
 #formulaS = @formula(0 ~ 1 +luminance + contrast + saccade_amplitude + string + animal + fruit + color)
 formulaS = @formula(0 ~ 1 + luminance + contrast + fruit + color)
 formulaS = @formula(0 ~ 1 + luminance + fruit)
@@ -9,10 +15,7 @@ dataS, evts = gen_data()
 times = range(0, length=size(dataS, 2), step=1 ./ 100)
 model = Unfold.fit(UnfoldModel, formulaS, evts, dataS, times)
 
-
 explore(model)
-
-
 
 #---
 
