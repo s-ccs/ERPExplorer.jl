b1af1dc343b468952e43628e0b0cba34a4f04bb4
Vladimir Mikheev <dezem@bk.ru>
2025-01-07 11:53:38 +0100

    better index

--- scripts/example.jl
+++ scripts/example.jl
@@ -13,6 +13,7 @@ begin
     using TopoPlots
 end
 
+
 include("gen_data.jl")
 #formulaS = @formula(0 ~ 1 +luminance + contrast + saccade_amplitude + string + animal + fruit + color)
 formulaS = @formula(0 ~ 1 + animal + fruit)
@@ -25,3 +26,29 @@ _, positions = TopoPlots.example_data()
 explore(model; positions = positions)
 
 #format_file("scripts/gen_data.jl")
+begin
+    using Pkg
+    Pkg.activate(".")
+    Pkg.status()
+end
+
+include("/store/users/mikheev/projects/erpexplorer_dev/dev/ERPExplorer/docs/make.jl")
+
+using JuliaFormatter
+begin
+    test_entries = readdir("./test")
+    cd("./test")
+    for i in test_entries
+        format_file(i)
+    end
+    src_entries = readdir("../src")
+    cd("../src")
+    for i in src_entries
+        format_file(i)
+    end
+    docs_entries = readdir("../docs")
+    cd("../docs")
+    for i in docs_entries
+        format_file(i)
+    end
+end
